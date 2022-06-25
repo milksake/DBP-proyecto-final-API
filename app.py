0e99b4e0ae71e402f0b2d39795c44b511e37b29a
milksake <caceresmariana443@gmail.com>
2022-06-25 18:35:20 -0500

    API and index.html (javascript)

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+import API
+
+app = Flask(__name__)
+
+app.config["DATABASE"] = "data.db"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# API functions
+
+@app.route('/get-all-users')
+def get_all_users():
+    with app.app_context():
+        u = API.get_users()
+    return u
+
+@app.route('/get-all-products')
+def get_all_products():
+    with app.app_context():
+        p = API.get_products()
+    return p
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8000, debug=True)
+    with app.app_context():
+        API.close_db()
